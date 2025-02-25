aed9b5694bcdc2f68b1c80c9c28f0b0cf43b44df
itsbillyherexd <khoanqase180654@fpt.edu.vn>
2025-02-25 15:07:15 +0700

    feat: adjust What component

--- src/components/What.jsx
+++ src/components/What.jsx
@@ -3,34 +3,34 @@ import grid from '../assets/grid.jpg';
 
 const What = () => {
   return (
-    <div></div>
-    // <div className="relative w-full h-screen my-32">
-    //     <div className="absolute top-0 left-0 w-full h-1/2 opacity-5">
-    //         <img src={grid} className="w-full h-full object-contain" />
-    //         <img src={grid} className="w-full h-full object-contain" />
-    //         <img src={grid} className="w-full h-full object-contain" />
-    //         <img src={grid} className="w-full h-full object-contain" />
-    //     </div>
+    <div className="relative w-full h-screen my-32">
+        <div className="absolute top-0 left-0 w-full h-[50vh] md:h-[75vh] opacity-5 flex flex-col">
+            <img src={grid} className="w-full h-full object-contain" />
+            <img src={grid} className="w-full h-full object-contain" />
+            <img src={grid} className="w-full h-full object-contain" />
+            <img src={grid} className="w-full h-full object-contain" />
+            <img src={grid} className="w-full h-full object-contain block md:hidden" />
+        </div>
 
-    //     <div className='grid lg:grid-cols-2 grid-cols-1  text-white lg:gap-x-60 mt-20 lg:mx-60 m-auto'>
-    //         <div className=' flex flex-col max-w-[400px] lg:mt-32 lg:mx-5 md:mx-auto'>
-    //             <h1 className='text-[min(5vw,3.5rem)] font-bold'>What's Layer</h1>
-    //             <p className='text-gray-400 text-lg font-thin mt-2'>LayerEdge is the first decentralized network that enhances the capabilities of Bitcoin Blockspace with ZK & BitVM.</p>
-    //         </div>
-    //         <div className='relative flex flex-col max-w-[400px] lg:mt-80 lg:mx-5 md:mx-auto'>
-    //             <h1 className='text-[min(5vw,3.5rem)] font-bold'>The Problem</h1>
-    //             <p className='text-gray-400 text-lg font-thin mt-2'>Most scaling solutions on Bitcoin are sidechains, while almost no blockchains or rollups currently settles on Bitcoin.</p>
-    //         </div>
-    //         <div className='relative flex flex-col max-w-[400px] lg:mt-10 lg:mx-5 md:mx-auto'>
-    //             <h1 className='text-[min(5vw,3.5rem)] font-bold'>Solution</h1>
-    //             <p className='text-gray-400 text-lg font-thin mt-2'>LayerEdge utilizes BitVM for Trust-minimized settlement on Bitcoin, while leveraging proof aggregation of zk proofs from various blockchains and rollups, enabling them to tap into Bitcoin's security by settling at up to 1% of the cost.</p>
-    //         </div>
-    //         <div className='relative flex flex-col max-w-[400px] lg:mt-80 lg:mx-5 md:mx-auto'>
-    //             <h1 className='text-[min(5vw,3.5rem)] font-bold'>Mission</h1>
-    //             <p className='text-gray-400 text-lg font-thin mt-2'>Unlocking Bitcoin as a bedrock for all blockchains/rollups and apps.</p>
-    //         </div>
-    //     </div>
-    // </div>
+        <div className='grid lg:grid-cols-2 grid-cols-1  text-white lg:gap-x-60 mt-20 lg:mx-60 m-auto'>
+            <div className=' flex flex-col max-w-[400px] lg:text-left  lg:mt-32 lg:mx-5 sm:mx-auto mx-32'>
+                <h1 className='text-[min(6vw,3.5rem)] font-bold'>What's Layer</h1>
+                <p className='text-gray-400 text-lg font-thin mt-2'>LayerEdge is the first decentralized network that enhances the capabilities of Bitcoin Blockspace with ZK & BitVM.</p>
+            </div>
+            <div className='relative flex flex-col max-w-[400px] lg:mt-80 lg:mx-5 sm:mx-auto mx-32'>
+                <h1 className='text-[min(5vw,3.5rem)] font-bold'>The Problem</h1>
+                <p className='text-gray-400 text-lg font-thin mt-2'>Most scaling solutions on Bitcoin are sidechains, while almost no blockchains or rollups currently settles on Bitcoin.</p>
+            </div>
+            <div className='relative flex flex-col max-w-[400px] lg:mt-10 lg:mx-5 sm:mx-auto mx-32'>
+                <h1 className='text-[min(5vw,3.5rem)] font-bold'>Solution</h1>
+                <p className='text-gray-400 text-lg font-thin mt-2'>LayerEdge utilizes BitVM for Trust-minimized settlement on Bitcoin, while leveraging proof aggregation of zk proofs from various blockchains and rollups, enabling them to tap into Bitcoin's security by settling at up to 1% of the cost.</p>
+            </div>
+            <div className='relative flex flex-col max-w-[400px] lg:mt-80 lg:mx-5 sm:mx-auto mx-32'>
+                <h1 className='text-[min(5vw,3.5rem)] font-bold'>Mission</h1>
+                <p className='text-gray-400 text-lg font-thin mt-2'>Unlocking Bitcoin as a bedrock for all blockchains/rollups and apps.</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
